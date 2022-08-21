d74e7dbea41328beddec5ddb84d3090cd25d2ec0
Ahmed Fasih <wuzzyview@gmail.com>
2022-08-21 11:03:34 -0700

    refactor to prepare to use routing

--- utils.ts
+++ utils.ts
@@ -13,3 +13,22 @@ export function groupBy<T, U>(arr: T[], f: (x: T) => U): Map<U, T[]> {
   }
   return ret;
 }
+
+export function substringInArray(v: string[], target: string): undefined|{
+  startIdx: number;
+  endIdx: number
+}
+{
+  // this is a prefix scan of `v`'s elements' lengths
+  const cumLengths = v.map((s) => s.length)
+                         .reduce((p, x) => p.concat(x + p[p.length - 1]), [0]);
+  const haystack = v.join('');
+  const match = haystack.indexOf(target);
+  if (match >= 0) {
+    const startIdx = cumLengths.indexOf(match);
+    const endIdx = cumLengths.indexOf(match + target.length);
+    if (startIdx >= 0 && endIdx >= 0) {
+      return {startIdx, endIdx};
+    }
+  }
+}
